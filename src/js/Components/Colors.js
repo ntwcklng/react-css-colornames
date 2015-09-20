a2c187d070a8b0a073aaab271d4edf7cab402db5
ntwcklng <ntwcklng@gmail.com>
2015-09-20 18:00:11 +0200

    recently picked

--- src/js/Components/Colors.js
+++ src/js/Components/Colors.js
@@ -1,22 +1,32 @@
 var React = require('react');
-var ReactAddons = require('react/addons')
 var ColorList = require('./ColorList');
 var rgbToHex = require('./Functions/RGBToHex');
 var ColorItem = require('./ColorItem');
 var Filter = require('./Filter');
-var ReactCSSTransitionGroup = ReactAddons.addons.CSSTransitionGroup;
 
 var Colors = React.createClass({
   getInitialState: function() {
     return {
-      backgroundColor: '#ff6347',
-      colorName: 'Tomato',
-      headerTextColor: '#1b1b1b',
-      hex: '#ff6347',
-      filter: false
+      // backgroundColor: '#ff6347',
+      // colorName: 'Tomato',
+      // headerTextColor: '#1b1b1b',
+      // hex: '#ff6347',
+      filter: false,
+      selection: []
     }
   },
-  showHexColor: function(colorName, textColor, hex) {
+  appendSelection: function(colorName) {
+    var newSelection = this.state.selection;
+    var tmp = newSelection.push(colorName);
+    if(newSelection.length > 5) {
+      newSelection.shift();
+    }
+    this.setState({
+      selection: newSelection
+    });
+  },
+  colorItemClicked: function(colorName, textColor, hex) {
+    this.appendSelection(colorName);
     this.setState({
       backgroundColor: hex,
       colorName: colorName,
@@ -39,16 +49,22 @@ var Colors = React.createClass({
     var activeFilter = this.state.filter;
     var renderColors = ColorList.map(function(color) {
       var textColor = color.textColor || "#1b1b1b";
-      return <ColorItem textColor={textColor} colorItemClickHandle={self.showHexColor} key={color.hex + color.name} colorname={color.name} hex={color.hex} groups={color.groups} filter={activeFilter} />
+      return <ColorItem textColor={textColor} colorItemClickHandle={self.colorItemClicked} key={color.hex + color.name} colorname={color.name} hex={color.hex} groups={color.groups} filter={activeFilter} />
     });
     var styles = {
       backgroundColor: this.state.backgroundColor,
       color: this.state.headerTextColor
     };
+    var stateSelection = this.state.selection;
+    console.log(stateSelection);
+    var renderSelection = stateSelection.join(', ');
     return (
       <div className="container">
-        <h2 className='headerColor' style={styles}>{this.state.hex} - <span className='textCapitalize'>{this.state.colorName}</span></h2>
+        {stateSelection.length >= 1 && (
+          <h2 className='headerColor' style={styles}>{this.state.hex} - <span className='textCapitalize'>{this.state.colorName}</span></h2>
+        )}
         <p>Click on a Color or Filter them by Colorgroups.</p>
+        <h5 className='textCapitalize recentlyPicked'>{stateSelection.length >= 1 && 'Recently picked: ' + renderSelection}</h5>
         <Filter activeFilter={activeFilter} setFilter={this.updateFilter} />
           {renderColors}
       </div>
